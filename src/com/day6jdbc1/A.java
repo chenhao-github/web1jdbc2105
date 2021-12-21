d116ca55ec53a6f30a9249f820e9c31814b1c561
chenhao-github <342735005@qq.com>
2021-12-21 10:38:16 +0800

    Update A.java
    添加了打印 222  333

--- src/com/day6jdbc1/A.java
+++ src/com/day6jdbc1/A.java
@@ -3,5 +3,7 @@ package com.day6jdbc1;
 public class A {
     public static void main(String[] args) {
         System.out.println(111);
+        System.out.println(222);
+        System.out.println(333);
     }
 }
